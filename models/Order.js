55ea62330112a67c8b06868b37672bd77c4e50d3
ruban57 <rubann567@gmail.com>
2025-05-06 11:14:15 +0300

    ordercreation

--- models/Order.js
+++ models/Order.js
@@ -1,15 +1,31 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    items: [
-      {
-        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
-        quantity: Number
-      }
-    ],
-    totalAmount: Number,
-    status: { type: String, default: 'Pending' }
-  });
-  
-  module.exports = mongoose.model('Order', orderSchema);
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  products: [
+    {
+      product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+      quantity: Number
+    }
+  ],
+  totalAmount: Number,
+  paymentMethod: {
+    type: String,
+    enum: ['Cash on Delivery', 'Credit Card', 'Apple Pay'],
+    default: 'Cash on Delivery'
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'Confirmed', 'Shipped', 'Delivered'],
+    default: 'Pending'
+  },
+  shippingAddress: {
+    addressLine: String,
+    city: String,
+    country: String,
+    postalCode: String
+  },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
